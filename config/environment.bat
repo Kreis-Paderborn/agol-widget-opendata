2146691a2297b90fd1c5e4726f42c0d902e20a4d
Trantow <trantow@kreispb.gkd>
2018-08-29 17:52:17 +0200

    Fläche prüfen per SOAPUI testen

--- config/environment.bat
+++ config/environment.bat
@@ -2,8 +2,8 @@
 REM Central definition of unique suffix for log files
 @for /F "tokens=1,2" %%d in ('date /T') do set xdate=%%d
 @for /F "tokens=1,2" %%e in ('time /T') do set xtime=%%e
-SET ENV_LOGDATE=%xDATE:~6,4%%xDATE:~3,2%%xDATE:~0,2%
-SET ENV_LOGDATETIME=%xDATE:~6,4%%xDATE:~3,2%%xDATE:~0,2%-%xtime:~0,2%%xtime:~3,2%
+SET ENV_LOGDATE=%xDATE:~6,4%-%xDATE:~3,2%-%xDATE:~0,2%
+SET ENV_LOGDATETIME=%xDATE:~6,4%-%xDATE:~3,2%-%xDATE:~0,2%_%xtime:~0,2%-%xtime:~3,2%
 
 SET WIDGET_NAME=OpenData Liegenschaftskarte
 
---
+++ tests/soapui/run_all_tests.bat
@@ -0,0 +1,24 @@
+@echo off
+
+REM Define general settings
+SET TEST_DIR=%~dp0
+SET ENDPOINT=https://fme.gkdpb.de
+SET BASE_DIR=%~dp0..\..\
+CALL %BASE_DIR%\config\environment.bat
+
+REM Relative dir for soap-ui Logs (used as well by junit-viewer)
+SET SOAP_LOG_DIR_NAME=logs\%ENV_LOGDATETIME%\
+
+REM Absoute path for soap-ui needed by Soap-UI itself
+SET MY_LOGDIR=%~dp0%SOAP_LOG_DIR_NAME%
+mkdir %MY_LOGDIR%
+
+REM RUN the SOAP-UI tests
+SET PROJECT_FILE="%~dp0Check-Anfrage-Flaeche-pruefen-soapui-project.xml"
+cd %MY_LOGDIR%
+call "C:\Programme portable\SoapUI-5.4.0-EB\bin\testrunner.bat" -e%ENDPOINT% -PFME_SERVER_TOKEN=%FME_SERVER_TOKEN% -r -j -f%MY_LOGDIR% %PROJECT_FILE% > %MY_LOGDIR%test_run.log
+cd %TEST_DIR%
+
+REM Create HTML out of soap-ui's junit results
+call junit-viewer --results=%SOAP_LOG_DIR_NAME% --save=%SOAP_LOG_DIR_NAME%results.html --minify=false  --contracted
+
